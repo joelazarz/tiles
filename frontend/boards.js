094af3b86dae5a6e7af72ff1f2b88f13054fbadc
joe lazarz <jlazarz87@gmail.com>
2019-08-19 18:42:24 -0400

    getting started

--- frontend/boards.js
+++ frontend/boards.js
@@ -1,9 +1,49 @@
 class Boards{
 
-    getBoards() {
+    getBoards(){
         return fetch('http://localhost:3000/api/boards')
         .then(resp => resp.json())
         .then(json => console.log(json));
+    }
+
+    // create board POST fetch - for seeding
+    createBoard(form){
+        const boardNameInput = document.querySelector('.board-name')
+        const boardDescriptionInput = document.querySelector('.board-description')
+        fetch("http://localhost:3000/api/boards",{
+            method: "POST",
+            headers:
+            {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            },
+            body: JSON.stringify({
+                name: boardNameInput.value, 
+                description: boardDescriptionInput.value})
+        })
+        .then(response => response.json())
+        form.reset()
+    }
+
+    // create block POST fetch - for seeding
+    createBlock(form){
+        const blockDescriptionInput = document.querySelector('.block-description')
+        const blockPhotoInput = document.querySelector('.block-photo')
+        const blockBoardIdInput = document.querySelector('.board-id-description')
+        fetch("http://localhost:3000/api/blocks",{
+            method: "POST",
+            headers:
+            {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            },
+            body: JSON.stringify({
+                description: blockDescriptionInput.value,
+                photo: blockPhotoInput.value, 
+                board_id: blockBoardIdInput.value})
+        })
+        .then(response => response.json())
+        form.reset()
     }
 
 }
